b63fcde92e49aadaeda18872ba99b371c488d310
Ali Rizvi <aliabbasrizvi@gmail.com>
2013-01-27 05:33:21 -0800

    added printability to the dump script and added a hack for url to not to confuse curl when multiple options are specified'

--- lib/sprintly.rb
+++ lib/sprintly.rb
@@ -8,31 +8,45 @@ class Sprintly
   end
 
   def base_url
-  	"#{user}:#{api_key} https://sprint.ly/api/"
+  	"#{user}:#{api_key} 'https://sprint.ly/api/"
   end
 
   def products_url(product_id)
   	base_url + 'products/' + product_id.to_s + "/"
   end
 
-  def product_items_url(product_id, options={})
-  	url = products_url(product_id) + 'items.json'
+  def product_items_url(product_id, options={}, format='json')
+  	url = products_url(product_id) + 'items.' + format
     if !options.empty?
       url += "?"
       options.each do |key, value|
         url += "#{key}=#{value}"
         url += "&"
       end
+      url.chop!
     end
-    url
+    url += "'"
   end
 
   # TODO: Make this method testable without depending on external service
   # Returns an ruby Array of Sprint.ly Items
   def items(product_id, options)
-    cmd = "curl -s -u #{product_items_url(product_id, options)}"
-    result = `#{cmd}`
-    JSON.parse(result)
+    JSON.parse(items_json(product_id, options))
+  end
+
+  def items_json(product_id, options)
+    content_fetcher(product_items_url(product_id, options))
+  end
+
+  def items_csv(product_id, options)
+    content_fetcher(product_items_url(product_id, options, 'csv'))
+  end
+
+  # if we make content fetcher an external object and pass it as initialize
+  # argument then we can inject the dependency to increase testability
+  def content_fetcher(sprintly_url)
+    cmd = "curl -s -u #{sprintly_url}"
+    `#{cmd}`
   end
 
 end
--- sprintly_dump.rb
+++ sprintly_dump.rb
@@ -6,6 +6,36 @@ $user = ENV['SPRINTLY_USER']
 $password = ENV['SPRINTLY_API_KEY']
 $product_id = ENV['SPRINTLY_PRODUCT_ID']
 
+$COLUMNS = %w(number type status title score tags last_modified assigned_to created_at created_by short_url email product description)
+
 sprintly = Sprintly.new($user, $password)
-pp sprintly.items($product_id, :limit => 2)
-#puts sprintly.items_json($product_id, :limit => 5).size
+
+def print_header
+  $COLUMNS.each do |column|
+    print column + ' | '
+  end
+  puts
+end
+
+def print_item(item)
+  $COLUMNS.each do |column|
+    print item[column].inspect
+    print ' | '
+  end
+  puts
+end
+
+def print_all(items)
+  items.each do |item|
+    print_item(item)
+  end
+end
+
+print_header
+offset = 0
+
+begin
+  items = sprintly.items($product_id, {:status => 'backlog,in-progress,completed,accepted', :offset => offset})
+  print_all(items)
+  offset += items.size
+end while items.size > 0
